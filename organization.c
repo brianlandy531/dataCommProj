26cd4196e0a5e05f7adb4982d0114e81a7e95e75
bxl1703 <bxl1703@cs.rit.edu>
2019-10-05 16:43:07 -0400

    bap bap

---
+++ organization.c
@@ -0,0 +1,11 @@
+//This file contains the main setup for the task
+
+
+//Will have a server object
+//A client object
+
+//A thread object
+//which has a client object and a server object that extends them
+
+//The main will respond to input and creat child threads to respond to the
+//clients. 
