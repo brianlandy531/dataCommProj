4e9a54397c4c8c031f43fe2e21865f0a19dbd446
brianlandy531 <brianlandy531@gmail.com>
2019-10-16 07:35:23 -0700

    beep

--- tigerC.c
+++ tigerC.c
@@ -12,41 +12,22 @@
 #define TEST_PORT 8080
 
 
-int main(int argc, char *argv[])
+int printTokens( char *inbuff)
 {
 
-	char inbuff[STR_MAX_LEN] = "";
-	char boof[STR_MAX_LEN] = "yessir a b c";
-
-    printf("Program name %s\n", argv[0]);
-
-    if (argc == 4 || argc == 2)  
-    { 
-
-        for(int j =0; j<argc; j++)
-        {
-            printf("[%d]: ", j);
-
-            printf("%s\n", argv[j]);
-        }
-
-}
-//char *fgets(char *str, int n, FILE *stream)
-
-	printf("%s\n", argv);
-
-	int counter =0;
-	
+    //char inbuff[STR_MAX_LEN] = "";
+    char toks[STR_MAX_LEN] = "";
+    int counter =0; 
     char delims[] = " \n";
 
-	//if correct num args
-  	if (argc == 4 || argc == 2)  
-    { 
-        
-    	strncpy(inbuff, boof, STR_MAX_LEN);
-       
-    	//printf("%s", inbuff);
-        char* token = strtok(inbuff, delims); 
+    //if correct num args
+    //if (argc == 4 || argc == 2)  
+    //{ 
+    
+    strncpy(toks, inbuff, STR_MAX_LEN);
+   
+    //printf("%s", inbuff);
+    char* token = strtok(toks, delims); 
   
     // Keep printing tokens while one of the 
     // delimiters present in str[]. 
@@ -55,16 +36,54 @@ int main(int argc, char *argv[])
         token = strtok(NULL, delims); 
     } 
 
-    	//Chose operating mode (connect, get put)
+        //Chose operating mode (connect, get put)
 
-    } 
-    else
-    {
-    	printf("enter 4 arguments for connect or 2 arguments for get or put\n"); 
-        return 0; 
-    }
+    //} 
+    //else
+    //{
+    //  printf("enter 4 arguments for connect or 2 arguments for get or put\n"); 
+    //    return 0; 
+    //}
 
   
     return 0; 
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+	char inbuff[STR_MAX_LEN] = "";
+	char boof[STR_MAX_LEN] = "yessir a b c";
+
+    char currIn[STR_MAX_LEN] = "";
+
+    printf("Program name %s\n", argv[0]);
+
+    char delims[] = " \n";
+    
+    while(fgets(inbuff, STR_MAX_LEN , stdin) != NULL)
+    {
+        //printf("%s\n", inbuff);
+    
+        printTokens(inbuff);
+
+
+
+    /*if (argc == 4 || argc == 2)  
+    { 
+
+        for(int j =0; j<argc; j++)
+        {
+            printf("[%d]: ", j);
+
+            printf("%s\n", argv[j]);
+        }
+    }*/
+
+    }
+
+
 
 }
